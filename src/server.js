e1359536fa06ea64470f458ac51d17ebb5c685b1
Luis Perez <44172765+Lap343@users.noreply.github.com>
2022-02-10 10:50:10 -0800

    look inside express docs and refactor some code

--- src/server.js
+++ src/server.js
@@ -1,18 +1,15 @@
-const http = require('http');
 const express = require('express');
 
 const app = express();
 
-app.use((req, res, next)=> {
+app.use((req, res, next) => {
     console.log("I'm in the middleware");
     // Allows the next middleware to fire off
     next();
 });
-app.use((req, res, next)=> {
+app.use((req, res, next) => {
     console.log("I am also in the middleware");
     res.send('<h1>Hello from Express!</h1>');
 });
 
-const server = http.createServer(app);
-
-server.listen(3000);
+app.listen(3000);
